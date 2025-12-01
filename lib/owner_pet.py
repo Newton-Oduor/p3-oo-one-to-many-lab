b3a69ffd0b2d31f3b15c5003d6fe9e3b9474cd5a
Newton Oduor <newtonochieng@gmail.com>
2025-12-01 11:24:35 +0300

    Lab done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,42 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError(
+                f"Invalid pet type. Must be one of {Pet.PET_TYPES}")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None
+        Pet.all.append(self)
+
+        if owner:
+            self.set_owner(owner)
+
+    def set_owner(self, owner):
+        """Assign an owner to this pet and add pet to owner's list"""
+        if hasattr(owner, "add_pet"):
+            self.owner = owner
+            owner.add_pet(self)
+        else:
+            raise TypeError("Owner must have an add_pet method")        
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Must be an instance of Pet")
+        if pet not in self._pets:
+            self._pets.append(pet)
+            pet.owner = self
+
+    def pets(self):
+        return self._pets
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
